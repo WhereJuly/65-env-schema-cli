20265d3e002c3f5421c44c427f22aebd7a8f0175
WhereJuly <where.july@googlemail.com>
2025-02-15 07:22:53 +0300

    [package] Construct (core): arrange and clean code

--- src/core/EnvSchemaCore.service.ts
+++ src/core/EnvSchemaCore.service.ts
@@ -57,10 +57,6 @@ export default class EnvSchemaCoreService {
         }
     }
 
-    private shouldLoadSchema(): boolean {
-        return this.#schema.isFileOrURL && !this.#schema.isLoaded;
-    }
-
     /**
      * IMPORTANT: Running `envSchema` throws if the env value is missing or does not match schema.
      * If operated on `process.env` it seems to unset loaded variables if the validation fails.
@@ -69,7 +65,7 @@ export default class EnvSchemaCoreService {
         /**
          * WARNING: 1) We pass `processEnv: destinationEnvVariables` to not write to `process.env`;
          * WARNING: 2) Why we have to pass `data: destinationEnvVariables` second time is not clear from 
-         * `env-schema` docs but it the only way it works.
+         * `env-schema` docs but it is the only way it works. Well see to it later.
          */
         const destinationEnvVariables = {};
         const dotEnvConfig = {
@@ -100,15 +96,20 @@ export default class EnvSchemaCoreService {
 
     private constructFullFilePathOrThrow(envFilePath?: string): string {
         const defaultEnvFile = '.env';
-
         const fullPath = path.resolve(process.cwd(), envFilePath ?? defaultEnvFile);
 
-        // WRITE: assert;
         if (!fs.existsSync(fullPath)) {
             throw new EnvSchemaCLIException(`The file at given 'envFilePath' "${fullPath}" does not exist.`);
         }
 
         return fullPath;
+    }
+
+    /**
+     * Check if schema should be loaded at all that it was not loaded already.
+     */
+    private shouldLoadSchema(): boolean {
+        return this.#schema.isFileOrURL && !this.#schema.isLoaded;
     }
 
     private async loadSchemaOrThrow(): Promise<Record<string, any>> {
